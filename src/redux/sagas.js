5bca48e233f99ba26d5299dbc967ada81cfbd117
George Campbell <campbell.goe@gmail.com>
2019-04-29 16:48:02 +0100

    minor refactor of sagas.js to remove duplicate code

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,4 +1,3 @@
-//TODO: refactor this file
 import { put, takeEvery, takeLatest, all, call } from 'redux-saga/effects';
 import { setRandomColour, SET_RANDOM_COLOUR_DELAYED } from './actions';
 
@@ -6,14 +5,16 @@ const delay = (ms) => new Promise(res => setTimeout(res, ms));
 
 export function* setRandomColourGeneratorLatest({ payload: { id, ms, sagaType } = {}} = {}){
   if(sagaType !== 'takeLatest') return;
-  console.log("set random colour", id, ms);
-  yield call(delay, ms);
-  yield put(setRandomColour(id));
+  yield call(setRandomColourDelayed, id, ms);
 }
 export function* setRandomColourGeneratorEvery({ payload: { id, ms, sagaType } = {}} = {}){
   if(sagaType !== 'takeEvery') return;
-  console.log("set random colour", id, ms);
+  yield call(setRandomColourDelayed, id, ms);
+}
+
+export function* setRandomColourDelayed(id, ms){
   yield call(delay, ms);
+  console.log("set random colour", id, ms);
   yield put(setRandomColour(id));
 }
 
